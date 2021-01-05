771240167b915251732bd0bb3961bf55f7d74df6
MauroPdz <mauropedrozo1@gmail.com>
2021-01-05 14:55:56 -0300

    Nuevos archivos

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 //Configuro el directorio de recursos estáticos
 app.use(express.static('public'));
 
+/* Levantar el servidor */
+
+app.listen(process.env.PORT || 3000, () => console.log('El servidor está corriendo en el puerto 3000'));
+
+/* Rutas a los recursos */
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'));
@@ -19,5 +24,4 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
-app.listen(3000, () => console.log('El servidor está corriendo en el puerto 3000'));
 
